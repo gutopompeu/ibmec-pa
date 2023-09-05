3d1817ca54f257ac15832726fc08199c21e234c4
gutopompeu <141173902+gutopompeu@users.noreply.github.com>
2023-09-05 15:02:19 -0300

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+numero = 10
+
+nome = "joão"
+
+booleano= True
+
+a = 10
+b = 5
+soma = a + b
+subtração = a - b
+multiplicacao = a * b
+divisao = a / b
+
+va = True
+vb = False
+
+r1 = va and vb
+r2 = va or vb
+r3 = not va 
+
+j = "joão"
+m = "maria"
+
+r4 = j == m
+
+
